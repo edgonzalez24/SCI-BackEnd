e3ce4e336211e60a22d4422c39e4e6279db1c6d4
Edwin González <edgoca2498@gmail.com>
2020-10-10 15:05:55 -0600

    :building_construction: Make architectural changes EP Book

--- routes/book.js
+++ routes/book.js
@@ -4,10 +4,13 @@ const { addBook, allBook } = require('../controllers/bookController');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos')
 
-router.post('/add', [
-    check('title_book', 'El campo Title Book es obligatorio').not().isEmpty(),
-    validarCampos
-], addBook)
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+router.post('/add',
+    validarJWT, [
+        check('title_book', 'El campo Title Book es obligatorio').not().isEmpty(),
+        validarCampos
+    ], addBook)
 
 
 router.get('/all', allBook)
